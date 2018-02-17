49a3024f2f2d7e1a8460aa006860e7b54f2a0a2e
JohnPaulPagelerJr <harakido1@yahoo.com>
2018-02-17 03:13:41 -0500

    Working markdown app

--- App.js
+++ App.js
@@ -7,10 +7,28 @@ import FormControl from 'react-bootstrap/lib/FormControl'
 
 const marked = require("marked");
 class App extends Component {
+  state = {
+    markdown: " "
+  }
+  updateMarkdown = function(markdown) {
+    this.setState({markdown});
+  }
   render() {
+    let{ markdown } = this.state;
+    console.log(markdown);
     return (
-      <div className="App">
-
+      <div className="App container-fluid">
+      <div>
+        <FormGroup controlId="formControlsTextarea">
+          <ControlLabel><u><h1>Enter Markdown Input Below</h1></u></ControlLabel>
+          <FormControl componentClass="textarea" placeholder="Enter Markdown" value={markdown} onChange = 
+          {(event)=>this.updateMarkdown(event.target.value)}></FormControl>
+        </FormGroup>
+      </div>
+      <div>
+        <u><h1>Markdown Output Below</h1></u>
+        <div dangerouslySetInnerHTML = {{__html: marked(markdown)}}></div> 
+      </div>
       </div>
     );
   }
